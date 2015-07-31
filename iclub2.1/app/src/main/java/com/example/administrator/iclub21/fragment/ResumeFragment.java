ee1c061fc172339802cdfeda7b4ee783a99e58a1
xiaoshengping <1277600115@qq.com>
2015-07-31 14:31:10 +0800

    xiaoshengping7.31--14:30

--- iclub2.1/app/src/main/java/com/example/administrator/iclub21/fragment/ResumeFragment.java
+++ iclub2.1/app/src/main/java/com/example/administrator/iclub21/fragment/ResumeFragment.java
@@ -69,7 +69,11 @@ public class ResumeFragment extends Fragment implements View.OnClickListener,Pul
 
     }
 
-
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().finish();
+    }
 
     private void intiView(View view) {
         View addView=LayoutInflater.from(getActivity()).inflate(R.layout.add_resume_layout,null);
