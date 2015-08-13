0ea179f7ea8314c14350a15cd47e063efc2a34ea
xiaoshengping <1277600115@qq.com>
2015-08-13 16:37:08 +0800

    xiaoshengping 08-13--16:16

--- iclub2.1/app/src/main/java/com/example/administrator/iclub21/fragment/MineFragment.java
+++ iclub2.1/app/src/main/java/com/example/administrator/iclub21/fragment/MineFragment.java
@@ -29,9 +29,6 @@ import com.jeremy.Customer.R;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.onekeyshare.OnekeyShare;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -66,6 +63,7 @@ public class MineFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
             View view=inflater.inflate(R.layout.fragment_mine, container, false);
             ViewUtils.inject(this, view);
+
         init();
         return view;
     }
@@ -179,49 +177,16 @@ public class MineFragment extends Fragment implements View.OnClickListener {
             case R.id.share_sdk_layout:
 //                Intent shareItent=new Intent(getActivity(),ShareActivity.class);
 //                startActivity(shareItent);\
-                showShare();
+
                 break;
 
         }
     }
 
 
-    //分享
-    private void showShare() {
-        ShareSDK.initSDK(getActivity());
-        OnekeyShare oks = new OnekeyShare();
-        //关闭sso授权
-        oks.disableSSOWhenAuthorize();
 
-// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
-        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
-        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
-        //oks.setTitle(getString(R.string.share));
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        //oks.setTitleUrl("http://sharesdk.cn");
-        //oks.setTitleUrl("http://mob.com");
-        // text是分享文本，所有平台都需要这个字段
-        //oks.setText("分享文本");
-        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
-        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
-        //oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
 
-        oks.setTitle("分享标题--Title");
-        oks.setTitleUrl("http://mob.com");
-        oks.setText("分享测试文--Text");
-        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
-        // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl("http://sharesdk.cn");
-        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
-        oks.setComment("我是测试评论文本");
-        // site是分享此内容的网站名称，仅在QQ空间使用
-        oks.setSite(getString(R.string.app_name));
-        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
-        oks.setSiteUrl("http://sharesdk.cn");
 
-// 启动分享GUI
-        oks.show(getActivity());
-    }
     //对话框
     private void showExitGameAlert() {
         final AlertDialog dlg = new AlertDialog.Builder(getActivity()).create();
